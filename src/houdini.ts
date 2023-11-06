12dc5e925547f7b1765628919d88f06455570ed0
James Birtles <James.Birtles@peopleplus.co.uk>
2023-11-06 12:20:30 +0000

    add utility to create houdini config in project

--- src/houdini.ts
+++ src/houdini.ts
@@ -1,17 +1,7 @@
 import generate from 'houdini/codegen';
-// @ts-expect-error Despite having typings, typescript doesn't seem to be able find them correctly.
 import { getConfig } from 'houdini';
 
-export const houdiniConfig = {
-	schemaPath: 'src/graphql/**/*.graphql',
-	plugins: {
-		'houdini-svelte': {
-			client: './src/graphql/client.ts',
-		},
-	},
-};
-
 export async function codegen() {
-	const config = await getConfig(houdiniConfig);
+	const config = await getConfig();
 	await generate(config);
 }
